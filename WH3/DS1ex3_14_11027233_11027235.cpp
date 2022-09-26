ec5efcd1e11f0f71a0e00d62ffd3ec5f03268600
Godkiller <daniel29348679@gmail.com>
2022-09-26 12:57:16 +0800

    2022/09/26-daniel-3

--- WH3/DS1ex3_14_11027233_11027235.cpp
+++ WH3/DS1ex3_14_11027233_11027235.cpp
@@ -2,7 +2,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector <char> allowedcharvec = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '(', ')', ' '};
+vector <char> allowedcharvec = {'0', '1', '2', '3', '4', '5', '6',
+								'7', '8', '9', '+', '-', '*', '/',
+								'(', ')', ' '}; // all allowed char in legal algorithm
 
 
 int main()
@@ -38,21 +40,21 @@ int main()
 		}
 		if(k == 1)
 		{
-			int	   numofparenthesis = 0;
-			int	   lasttype			= 2;
-			bool   lastisnumer		= 0;
-			bool   allgood			= 1;
+			int	   numofparenthesis = 0; //number of '( - number of ')
+			int	   lasttype			= 2; //1->operand,2->operator
+			bool   lastisnumer		= 0; //true means last char is number
+			bool   allgood			= 1; //no error
 			string str;
-			cin.get();
+			cin.get();                   //after cin>>k left a '\n' so use cin.get() to clear it
 			cout << "Input:";
-			getline(cin, str);
+			getline(cin, str);           //read line
 			for(auto i:str)
 			{
-				if(find(allowedcharvec.begin(), allowedcharvec.end(), i) == allowedcharvec.end())
+				if(find(allowedcharvec.begin(), allowedcharvec.end(), i) == allowedcharvec.end()) //i must in allowedcharvec
 				{
 					cout << "Error 1: " << i << " is not a legitimate character.";
-					allgood = 0;
-					break;
+					allgood = 0; //set allgod to false
+					break;       //if found one so that don't have to fine another
 				}
 
 
@@ -95,31 +97,30 @@ int main()
 				if(i == ' ')
 					lastisnumer = 0;
 
-				if(nowtype != -1)
+				if(nowtype != -1)                           //if nowtype == -1 -> no new type found
 				{
-					if(nowtype == lasttype && nowtype == 1)
+					if(nowtype == lasttype && nowtype == 1) //operand after operand
 					{
 						cout << "Error 2: there is one extra operand.";
 						allgood = 0;
 						break;
 					}
-					if(nowtype == lasttype && nowtype == 2)
+					if(nowtype == lasttype && nowtype == 2) //operator after operator
 					{
 						cout << "Error 2: there is one extra operator.";
 						allgood = 0;
 						break;
 					}
-					lasttype = nowtype;
+					lasttype = nowtype; //if found new type than refresh lasttype
 				}
 			}
 
-			if(numofparenthesis > 0)
+			if(numofparenthesis > 0)//'(' - ')' after all must = 0
 			{
 				cout << "Error 2: there is one extra open parenthesis.";
 				allgood = 0;
-				continue;
 			}
-			if(allgood)
+			if(allgood) //no error cout right
 				cout << "It is a legitimate infix expression";
 
 			//(29+101)*33/25
@@ -136,53 +137,61 @@ int main()
 			getline(cin, str);
 			stack <char> operatorst;
 
-			int n = 0;
+			int	 n	   = 0;
+			bool isint = 0; //last is operand
 			for(auto i:str)
 			{
 				if(i == ' ')
-					continue;
-				if('0' <= i && i <= '9')
-					n = n * 10 + i - '0';
-
-				if(i == '+' || i == '-' || i == '*' || i == '/' || i == '(')
+					continue;                 //skip ' '
+				if('0' <= i && i <= '9')      //if i is number
 				{
-					if(n > 0)
-					{
-						cout << n << ',';
-						n = 0;
-					}
-					operatorst.push(i);
+					n	  = n * 10 + i - '0'; //add to n
+					isint = 1;                //set isint to true
 				}
 
-				if(i == ')')
+				if(i == '+' || i == '-' || i == '*' || i == '/' || i == '(') //operator
 				{
-					if(n > 0)
+					if(isint)
+					{
+						cout << n << ','; //cout operand
+						n	  = 0;
+						isint = 0;
+					}
+					operatorst.push(i); //push operator to stack
+				}
+
+				if(i == ')') //if find ')'
+				{
+					if(isint)
 					{
 						cout << n << ',';
-						n = 0;
+						n	  = 0;
+						isint = 0;
 					}
-					char c = operatorst.top();
-					operatorst.pop();
-					while(c != '(')
+
+					while(operatorst.top() != '(') //output stack until '('
 					{
-						cout << c << ',';
-						c = operatorst.top();
+						cout << operatorst.top() << ',';
 						operatorst.pop();
 					}
+					operatorst.pop(); //pop '('
 				}
 			}
-			if(n > 0)
+
+			if(isint) //output remained operand
 			{
 				cout << n << ',';
-				n = 0;
+				n	  = 0;
+				isint = 0;
 			}
-			while(operatorst.size())
+			while(operatorst.size()) //output remained operator
 			{
 				cout << operatorst.top() << ',';
 				operatorst.pop();
 			}
-			cout << '\b' << " ";
+			cout << '\b' << " "; //delete last ','
 		}
+
 
 		if(k == 3)
 		{
@@ -190,53 +199,56 @@ int main()
 			cin.get();
 			cout << "Input:";
 			getline(cin, str);
-			stack <int> operand;
+			stack <int> operandst;
 
-			int n = 0;
-			bool isint =0;
+			int	 n	   = 0;
+			bool isint = 0;
 			for(auto i:str)
 			{
 				if(i == ' ')
 					continue;
-				if('0' <= i && i <= '9'){
-					n = n * 10 + i - '0';
-					isint=1;
+				if('0' <= i && i <= '9')
+				{
+					n	  = n * 10 + i - '0';
+					isint = 1;
 				}
 
-				if(i == ','&&isint==1)
+				if(i == ',' && isint == 1) //last is number
 				{
-					operand.push(n);
-					n = 0;
-					isint=0;
+					operandst.push(n); //push opreand to stack
+					n	  = 0;
+					isint = 0;
 				}
 				if(i == '+' || i == '-' || i == '*' || i == '/')
 				{
-					int b = operand.top();
-					operand.pop();
-					int a = operand.top();
-					operand.pop();
+					//get a and b
+					int b = operandst.top(); //stack:1,2,3,4,5,6,a,b
+					operandst.pop();
+					int a = operandst.top(); //stack:1,2,3,4,5,6,a
+					operandst.pop();
 
+					//push a +-*/ b back into stack
 					if(i == '+')
-						operand.push(a + b);
+						operandst.push(a + b);
 					if(i == '-')
-						operand.push(a - b);
+						operandst.push(a - b);
 					if(i == '*')
-						operand.push(a * b);
-					if(i == '/'){
-						if(b!=0)
-							operand.push(a / b);
+						operandst.push(a * b);
+					if(i == '/')
+					{
+						if(b != 0) // b can't be 0
+							operandst.push(a / b);
 						else
 						{
-							cout<<"ERROR!!!! can't do /0";
-							goto quit;
+							cout << "ERROR!!!! can't do /0";
+							goto quit; //continue the while(k!=0)
 						}
 					}
 				}
 			}
 
-			cout <<"Answer: "<< operand.top();
-			while(operand.size())operand.pop();
-		quit:;
+			cout << "Answer: " << operandst.top(); //cout answer
+quit:       ; //quit at if(k==3) end
 		}
 	}
 }
