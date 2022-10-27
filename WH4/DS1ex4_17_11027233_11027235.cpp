805e29b8dcd2919c855d909d6acb31be03fe4ff2
Godkiller <daniel29348679@gmail.com>
2022-10-27 14:04:41 +0800

    2022/10/27-daniel-2

--- WH4/DS1ex4_17_11027233_11027235.cpp
+++ WH4/DS1ex4_17_11027233_11027235.cpp
@@ -23,33 +23,57 @@ public:
     int arrival;
     int duration;
     int timeout;
+    int startcooking;
+    int abort     = 0;
+    int delay     = 0;
+    int departure = 0;
+
+    string stat = "";
+    string cid;
 };
 
 
+ostream &operator<<(ostream&s, order&i)
+{
+    s << i.oid << '\t'
+      << i.arrival << '\t'
+      << i.duration << '\t'
+      << i.timeout << '\t'
+      << i.stat << '\t'
+      << i.cid << '\t'
+      << i.abort << '\t'
+      << i.delay << '\t'
+      << i.departure;
+    return s;
+}
+
 vector<order> ordervec;
-vector<order> finishvec;
-int           nowtime = 0;
+vector<order> cancelvec;
+//vector<order> finishvec;
+int nowtime = 0;
 void vecclear()
 {
     ordervec.clear();
-    finishvec.clear();
+    cancelvec.clear();
 }
 
 class chef
 {
 public:
 
-    int cheftime       = 0;
+    int cheftime = 0;
     string cid;
-    order ordqueue[3];
-    int index = 0;
-    int size  = 0;
+    int ordqueue[3];
+    int index    = 0;
+    int size     = 0;
+    bool cooking = 0;
+    int ing;
     chef(string s)
     {
         cid = s;
     }
 
-    void push(order o)
+    void push(int o)
     {
         if(size > 2)
             cout << "ERROR!!!!!!!!!!!!";
@@ -65,7 +89,7 @@ public:
         index++;
     }
 
-    order top(order)
+    int top()
     {
         if(size < 1)
             cout << "ERROR!!!!!!!!!!!!";
@@ -74,13 +98,48 @@ public:
 
     void cook()
     {
-    }
+        if(cooking)
+            if(nowtime >= ordervec[ing].startcooking + ordervec[ing].duration)
+            {
+                //煮完
+                if(ordervec[ing].startcooking + ordervec[ing].duration > ordervec[ing].timeout)
+                {
+                    ordervec[ing].stat  = "timeout";
+                    ordervec[ing].cid   = cid;
+                    ordervec[ing].delay = nowtime - ordervec[ing].arrival - ordervec[ing].duration;
+                    ordervec[ing].departure = nowtime;
+                    cancelvec.push_back(ordervec[ing]);
+                }
+                else
+                {
+                    ordervec[ing].stat = "success";
+                    ordervec[ing].cid  = cid;
+                }
+                cooking = 0;
+            }
 
-    int finishtime()
-    {
-        return 0;
+        if(!cooking) //收新的
+            while(size && !cooking)
+            {
+                ing = top();
+                pop();
+                if(nowtime >= ordervec[ing].arrival&&nowtime < ordervec[ing].timeout)
+                {
+                    cooking = 1;
+                    ordervec[ing].startcooking = nowtime;
+                }
+                else //if(nowtime > ordervec[ing].timeout)
+                {
+                    ordervec[ing].stat  = "abort";
+                    ordervec[ing].cid   = cid;
+                    ordervec[ing].delay = nowtime - ordervec[ing].arrival ;
+                    ordervec[ing].abort = nowtime;
+                    cancelvec.push_back(ordervec[ing]);
+                }
+            }
     }
 };
+
 
 ifstream &operator>>(ifstream&s, order&o)
 {
@@ -165,8 +224,8 @@ int main()
             if(!in)
                 cout << "error";
             getline(in, s);
-            order         o;
-            int           time = clock();
+            order o;
+            int   time = clock();
             while(in >> o)
                 ordervec.push_back(o);
             cout << "Sorting data:" << clock() - time << "ms\n";
@@ -193,16 +252,173 @@ int main()
         if(k == 2)
         {
             string name, s;
-            cout << "input:";
+            cout << "input:\n";
             cin >> name;
             ifstream in("sort" + name + ".txt");
             if(!in)
                 cout << "error";
             getline(in, s);
             order o;
-            int   time = clock();
             while(in >> o)
                 ordervec.push_back(o);
+
+            int  totalsize = ordervec.size();
+            chef chef1("chef1");
+            int  i = 0;
+            for(nowtime = 0 ; nowtime < 1000 ; nowtime++)
+            {
+                chef1.cook();
+                while(i < ordervec.size() && ordervec[i].arrival == nowtime)
+                {
+                    if(!chef1.cooking)
+                    {
+                        chef1.push(i);
+                        chef1.cook();
+                    }
+
+                    else if(chef1.size < 3)
+                        chef1.push(i);
+                    else
+                    {
+                        ordervec[i].stat  = "abort";
+                        ordervec[i].cid   = "0";
+                        ordervec[i].abort = ordervec[i].arrival;
+                        cancelvec.push_back(ordervec[i]);
+                    }
+                    i++;
+                }
+
+                chef1.cook();
+            }
+
+
+            for(auto i:ordervec)
+                cout << i << '\n';
+
+            string ans   = "";
+            int    delay = 0;
+
+            ans += "[Abort List]\n\tOID\tDelay\tAbort\n";
+            int ab = 0;
+            for(auto x:cancelvec | ranges::views::filter([](order o){
+                return o.stat == "abort";
+            }))
+            {
+                ans   += "[" + to_string(++ab) + "]\t" + to_string(x.oid) + "\t" + to_string(x.delay) + "\t" + to_string(x.abort) + "\n";
+                delay += x.delay;
+            }
+
+
+            ans += "[Timeout List]\n\tOID\tDelay\tDeparture\n";
+            int timo = 0;
+            for(auto x:cancelvec | ranges::views::filter([](order o){
+                return o.stat == "timeout";
+            }))
+            {
+                ans   += "[" + to_string(++ab) + "]\t" + to_string(x.oid) + "\t" + to_string(x.delay) + "\t" + to_string(x.departure) + "\n";
+                delay += x.delay;
+            }
+
+
+            ans += "[Total Delay]\n" + to_string(delay) + " min.\n";
+            ans += "[Failure Perecentage]\n" + to_string((float)100*(ab + timo) / totalsize).substr(0,to_string((float)100*(ab + timo) / totalsize).find(".")+3) + " %\n";
+
+            cout << ans;
+            ofstream out("one" + name + ".txt", ios::trunc);
+            out<<ans;
+            vecclear();
+        }
+
+        if(k == 3)
+        {
+            string name, s;
+            cout << "input:\n";
+            cin >> name;
+            ifstream in("sort" + name + ".txt");
+            if(!in)
+                cout << "error";
+            getline(in, s);
+            order o;
+            while(in >> o)
+                ordervec.push_back(o);
+
+            int  totalsize = ordervec.size();
+            chef chef1("1");
+            chef chef2("2");
+            int  i = 0;
+            for(nowtime = 0 ; nowtime < 1000 ; nowtime++)
+            {
+                chef1.cook();
+                chef2.cook();
+                while(i < ordervec.size() && ordervec[i].arrival == nowtime)
+                {
+                    if(!chef1.cooking)
+                    {
+                        chef1.push(i);
+                        chef1.cook();
+                    }
+                    else if(!chef2.cooking)
+                    {
+                        chef2.push(i);
+                        chef2.cook();
+                    }
+                    else if(chef1.size<chef2.size)
+                        chef1.push(i);
+                    else if(chef1.size>chef2.size)
+                        chef2.push(i);
+                    else if(chef1.size==chef2.size&&chef1.size < 3)
+                        chef1.push(i);
+                    else
+                    {
+                        ordervec[i].stat  = "abort";
+                        ordervec[i].cid   = "0";
+                        ordervec[i].abort = ordervec[i].arrival;
+                        cancelvec.push_back(ordervec[i]);
+                    }
+                    i++;
+                }
+
+                chef1.cook();
+                chef2.cook();
+            }
+
+
+            for(auto i:ordervec)
+                cout << i << '\n';
+
+            string ans   = "";
+            int    delay = 0;
+
+            ans += "[Abort List]\n\tOID\tCID\tDelay\tAbort\n";
+            int ab = 0;
+            for(auto x:cancelvec | ranges::views::filter([](order o){
+                return o.stat == "abort";
+            }))
+            {
+                ans   += "[" + to_string(++ab) + "]\t" + to_string(x.oid) + "\t"+x.cid+ "\t" + to_string(x.delay) + "\t" + to_string(x.abort) + "\n";
+                delay += x.delay;
+            }
+
+
+            ans += "[Timeout List]\n\tOID\tCID\tDelay\tDeparture\n";
+            int timo = 0;
+            for(auto x:cancelvec | ranges::views::filter([](order o){
+                return o.stat == "timeout";
+            }))
+            {
+                ans   += "[" + to_string(++ab) + "]\t" + to_string(x.oid) + "\t"+x.cid+"\t" + to_string(x.delay) + "\t" + to_string(x.departure) + "\n";
+                delay += x.delay;
+            }
+
+
+            ans += "[Total Delay]\n" + to_string(delay) + " min.\n";
+            ans += "[Failure Perecentage]\n" + to_string((float)100*(ab + timo) / totalsize).substr(0,to_string((float)100*(ab + timo) / totalsize).find(".")+3) + " %\n";
+
+            cout << ans;
+
+            ofstream out("two" + name + ".txt", ios::trunc);
+            out<<ans;
+            vecclear();
         }
     }
 }
